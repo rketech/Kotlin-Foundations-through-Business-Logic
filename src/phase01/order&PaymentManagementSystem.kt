d3194b72ddf8e5b68caa3ae775f7f662db699e2b
rketech <122847077+rketech@users.noreply.github.com>
2026-08-15 08:55:10 +0530

    Phase 01 / Day 12 / Order & Payment Management System (OPMS) / We're deliberately building the domain model first.

---
+++ src/phase01/order&PaymentManagementSystem.kt
@@ -0,0 +1,74 @@
+package phase01
+
+/*
+RK Order & Payment Management System (OPMS)
+
+1. Create Order
+2. Add Product
+3. View Order
+4. Calculate Total
+5. Make Payment
+6. View Order Status
+7. Exit
+*/
+
+/*
+Entity	        Property	Data Type	    val/var
+Customer_OPMS	name	    String	        val
+Customer_OPMS	phone	    String	        val
+Product_OPMS	name	    String	        val
+Product_OPMS	price	    Double	        val
+Product_OPMS	quantity	Int	            val
+Order_OPMS	    orderId	    String	        val
+Order_OPMS	    customer	Customer	    val
+Order_OPMS	    products	List<Product>	val
+Payment_OPMS	amount	    Double	        val
+Payment_OPMS	method	    String	        val
+
+Conceptually:
+
+Order_OPMS
+     │
+     ├──────────────► Customer_OPMS
+     │                   ├── name
+     │                   └── phone
+     │
+     └──────────────► List<Product_OPMS>
+                         │
+                         ├── Product_OPMS
+                         ├── Product_OPMS
+                         └── Product_OPMS
+
+This is a big step toward how real applications model data.
+*/
+
+
+data class Customer_OPMS(
+    val name: String,
+    val phone: String,
+)
+
+data class Product_OPMS(
+    val name: String,
+    val price: Double,
+    val quantity: Int,
+)
+
+data class Order_OPMS(
+    val order_ID: String,
+    val customer: Customer_OPMS,
+    val products: List<Product_OPMS>
+)
+
+enum class PaymentMethod {
+    CASH,
+    CARD,
+    UPI
+}
+
+data class Payment_OPMS(
+    val amount: Double,
+    val method: PaymentMethod
+)
+
+fun main(){}
